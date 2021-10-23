4c950eef857739d6d149d8b2b5f7b4c7702fb0ba
JisuKim <jso_osj@naver.com>
2021-10-24 02:11:18 +0900

    :sparkle: D-day 기능 구현!!!

--- d-day-counter/src/components/Text2.jsx
+++ d-day-counter/src/components/Text2.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Text2({year, month, date}) {
+  const [restDate, setRestDate] = useState("D-day는?");
+
+  const calcDday = (e) => {
+    //1. input에서 입력한 일수를 받아온다(dDay)
+    //2. baseDate에서 dDay를 뺀다 : setDate(date - dDay)
+
+    let dDay = Number(e.target.value);
+    let baseDate = new Date(year, month-1, date);
+
+
+    baseDate.setDate(date - dDay);
+    setRestDate(baseDate.toLocaleDateString());
+  }
+
   return (
+
+
     <div className="text__left">
       <div>
-        D - <input type="text" />은?
+        D - <input onChange={calcDday} type="text" />은?
       </div>
       <div>
-        yyyy년 mm월 dd일
+        {restDate}
       </div>
     </div>
   )
