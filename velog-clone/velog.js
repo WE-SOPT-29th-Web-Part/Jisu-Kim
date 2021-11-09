ad35a90b641436156f6cc5f208194ee608d898f0
JisuKim <jso_osj@naver.com>
2021-11-09 15:59:23 +0900

    :sparkle:local storage를 이용해 다크모드 구현,:art: 스위치버튼

--- velog-clone/velog.js
+++ velog-clone/velog.js
@@ -60,6 +60,26 @@ function createCloseBtn(modalBack) {
 }
 
 const darkModer = document.querySelector('#darkmode-checker');
+document.documentElement.setAttribute("color-theme", "light");//기본값 light로 설정해주기
+
+//local스토리지 정보가져오기
+const isUserColorTheme = localStorage.getItem('color-item');
+//prefers-color-scheme이 dark로 설정된 것이 true면 dark로 설정, 아님 light로
+const isOsColorTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+
+//localStorage정보가 있으면 그대로 사용하고, 없으면 OS정보를 사용
+const getUserTheme = () => (isUserColorTheme ? isUserColorTheme : isOsColorTheme);
+
+window.onload = function () {
+  if (getUserTheme === 'dark') {
+    localStorage.setItem('color-theme', 'dark');
+    document.documentElement.setAttribute('color-theme', 'dark');
+    darkModer.setAttribute('checked', true);
+  } else {
+    localStorage.setItem('color-theme', 'light');
+    document.documentElement.setAttribute('color-theme', 'light');
+  }
+};
 
 darkModer.addEventListener('click', e => {
   if (e.target.checked) {
